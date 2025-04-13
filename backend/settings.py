338ca5874cb8f4b92083b39319f0e0c788b3992d
Ashdixit382 <ashishdixit601@gmail.com>
2025-04-13 23:55:54 +0530

    some changes

--- backend/settings.py
+++ backend/settings.py
@@ -23,14 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-4@6v=s0l8s4pt9w$9z!g5ebku6bl4iuev16s-b7jgnr9zhl64y'
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-default-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost']
+# ALLOWED_HOSTS = ['.onrender.com', 'localhost']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",") 
 # ALLOWED_HOSTS = []
 
 
