0eee88aa86fb95ab7fccaceb38cd93e215dfc1a5
Ashdixit382 <ashishdixit601@gmail.com>
2025-04-13 09:17:26 +0530

    done

--- CodeQuest/backend/settings.py
+++ CodeQuest/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4@6v=s0l8s4pt9w$9z!g5ebku6bl4iuev16s-b7jgnr9zhl64y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codequest.onrender.com']
 
 
 # Application definition
@@ -128,6 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
