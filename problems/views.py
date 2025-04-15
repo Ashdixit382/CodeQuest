d671d0e2f3908d08dd9e2467d6a83cd08740fbd4
Ashdixit382 <ashishdixit601@gmail.com>
2025-04-15 14:30:32 +0530

    1 question deleted from db

--- problems/views.py
+++ problems/views.py
@@ -26,108 +26,6 @@ def register_user_handle(request):
     return Response({"message": f"{handle} registered successfully!"})
 
 
-# @api_view(['GET'])
-# def filter_problems(request):
-#     min_rating = request.query_params.get('min_rating')
-#     max_rating = request.query_params.get('max_rating')
-#     index = request.query_params.get('index')
-#     handle = request.query_params.get('handle')
-#     division = request.query_params.get('division')
-#     sort_by = request.query_params.get('sort_by')
-
-#     queryset = CodeforcesProblem.objects.all()
-
-#     # Apply rating range filters
-#     if min_rating and max_rating:
-#         queryset = queryset.filter(rating__gte=min_rating, rating__lte=max_rating)
-#     elif min_rating:
-#         queryset = queryset.filter(rating__gte=min_rating)
-#     elif max_rating:
-#         queryset = queryset.filter(rating__lte=max_rating)
-
-#     # Apply index filter
-#     if index:
-#         queryset = queryset.filter(index__startswith=index.upper())
-
-#     # Apply division filter
-#     if division:
-#         queryset = queryset.filter(division=division)
-
-#     # Solved problems check
-#     solved_ids = set()
-#     if handle:
-#         try:
-#             user = CodeforcesUser.objects.get(handle=handle)
-#         except CodeforcesUser.DoesNotExist:
-#             user = fetch_user_solved_problems(handle)
-#             if user is None:
-#                 return Response({"error": "Invalid handle or fetch failed"}, status=400)
-#         solved_ids = set(user.solved_problems.values_list('id', flat=True))
-
-#     # Apply sorting if required
-#     if sort_by:
-#         if sort_by == 'rating':
-#             queryset = queryset.order_by('rating')
-#         elif sort_by == 'index':
-#             queryset = queryset.order_by('index')
-
-#     results = []
-#     for prob in queryset:
-#         results.append({
-#             "name": prob.name,
-#             "contestId": prob.contest_id,
-#             "index": prob.index,
-#             "rating": prob.rating,
-#             "url": prob.url,
-#             "status": "solved" if prob.id in solved_ids else "unsolved"
-#         })
-
-#     return Response(results)
-
-
-
-
-
-# def filter_problems_page(request):
-#     min_rating = request.GET.get('min_rating')
-#     max_rating = request.GET.get('max_rating')
-#     index = request.GET.get('index')
-#     handle = request.GET.get('handle')
-#     division = request.GET.get('division')
-#     sort_by = request.GET.get('sort_by')
-
-#     params = {}
-#     if min_rating:
-#         params['min_rating'] = min_rating
-#     if max_rating:
-#         params['max_rating'] = max_rating
-#     if index:
-#         params['index'] = index
-#     if handle:
-#         params['handle'] = handle
-#     if division:
-#         params['division'] = division
-#     if sort_by:
-#         params['sort_by'] = sort_by
-
-#     problems = []
-#     if params:
-#         response = requests.get('https://codequest-ylrx.onrender.com/filter-problems/', params=params)
-#         if response.status_code == 200:
-#             problems = response.json()
-#     else:
-#         response = requests.get('https://codequest-ylrx.onrender.com/filter-problems/')
-#         if response.status_code == 200:
-#             problems = response.json()        
-
-#     divisions = ["Div. 1", "Div. 2", "Div. 3", "Div. 4", "Educational", "Global"]
-
-#     return render(request, 'filter_problems.html', {
-#         'problems': problems,
-#         'request': request,
-#         'divisions': divisions,
-#     })
-
 @api_view(['GET'])
 def filter_problems(request):
     min_rating = request.query_params.get('min_rating')
@@ -154,26 +52,6 @@ def filter_problems(request):
         })
 
     return Response(results)
-
-# def filter_problems_page(request):
-#     min_rating = request.GET.get('min_rating')
-#     max_rating = request.GET.get('max_rating')
-#     index = request.GET.get('index')
-#     handle = request.GET.get('handle')
-#     division = request.GET.get('division')
-#     sort_by = request.GET.get('sort_by')
-
-#     problems, solved_ids = filter_codeforces_problems(min_rating, max_rating, index, handle, division, sort_by)
-
-#     if problems is None:
-#         return render(request, 'filter_problems.html', {'error': 'Failed to fetch problems or invalid handle'})
-
-#     divisions = ["Div. 1", "Div. 2", "Div. 3", "Div. 4", "Educational", "Global"]
-#     return render(request, 'filter_problems.html', {
-#         'problems': problems,
-#         'request': request,
-#         'divisions': divisions,
-#     })
 
 def filter_problems_page(request):
     min_rating = request.GET.get('min_rating')
